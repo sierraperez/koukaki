86ac83d2938fe3f7059d7342101d6fc4d7fa9698
Eliezer Perez <eliezersierraperez@hotmail.com>
2024-06-06 12:54:18 +0200

    banner opti commit

--- wp-content/themes/foce-child/front-page.php
+++ wp-content/themes/foce-child/front-page.php
@@ -4,8 +4,11 @@ get_header();
 ?>
 
     <main id="primary" class="site-main">
-        <section class="banner">
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/logo.png'; ?> " alt="logo Fleurs d'oranger & chats errants">
+        <section  class="banner">
+            <video class="video" muted autoplay ='autoplay' loop='infinite' width="1440px">
+                <source src="<?php echo get_stylesheet_directory_uri() . '/assets/video/video.mp4'; ?>" type="video/mp4" />
+            </video>
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/logo.png'; ?> " alt="logo Fleurs d'oranger & chats errants">
         </section>
         <section id="#story" class="story">
             <h2>L'histoire</h2>
